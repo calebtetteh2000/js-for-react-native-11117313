0d1285e684869527cf2c1b07de3da687c617ae0f
Caleb-Tetteh <cotetteh003@st.ug.edu.gh>
2024-05-18 18:06:32 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,5 @@
-function processArray(numbers){
-    return numbers.map(function (number) {
+function processArray(num){
+    return num.map(function (number) {
         if(number % 2 === 0){
             return number * number;
         }else{
@@ -7,6 +7,31 @@ function processArray(numbers){
         }
     });
 }
-const numbers = [1,2,3,4,5];
-const processedNumbers = processArray(numbers);
+const num = [1,2,3,4,5];
+const processedNumbers = processArray(num);
 console.log(processedNumbers);
+
+function formatArrayStrings(stringArray, numberArray){
+    // Check if the lengths of the input arrays are the same
+    if (stringArray.length !== numberArray.length){
+        throw new Error('Array Inputs must have the same length');
+    }
+    // We Iterate over the arrays and modify the strings based on the numbers
+    for (let i = 0; i < stringArray.length; i ++){
+        const string = stringArray[i];
+        const number = numberArray[i];
+        // We Capitalise the string if the number is even
+        if (number % 2 === 0){
+            stringArray[i] = string.toUpperCase();
+        }
+        // Convert the string to lowercase if the number is odd
+        else{
+            stringArray[i] = string.toLowerCase();
+        }
+    }
+    return stringArray;
+}
+const strings = ['Mercedes', 'TOYOTA', 'FriedRice'];
+const numbers = [2, 5, 8];
+const formattedStrings = formatArrayStrings(strings, numbers);
+console.log(formattedStrings);
